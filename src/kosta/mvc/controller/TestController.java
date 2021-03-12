81815359c45241d69a2b0f0754054f7ff1f7a9af
l <l@DESKTOP-H5TMK7S>
2021-03-12 11:08:04 +0900

    두 번째

--- src/kosta/mvc/controller/TestController.java
+++ src/kosta/mvc/controller/TestController.java
@@ -2,4 +2,13 @@ package kosta.mvc.controller;
 
 public class TestController {
 
+	public void insert() {
+		System.out.println("Insert");
+	}
+	
+	public void select() {
+		System.out.println("Select");
+	}
+	
+	
 }
